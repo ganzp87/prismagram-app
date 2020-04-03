1d6d97796d96e3796c00448844bc1e29254aa8e3
Ganzp <ganzp@naver.com>
2020-04-03 21:21:43 +0900

    apollo uri

--- apollo.js
+++ apollo.js
@@ -1,5 +1,8 @@
 const options = {
-	uri: "http://192.168.0.3:4000"
+	uri:
+		process.env.NODE_ENV === "development"
+			? "http://192.168.0.3:4000"
+			: "https://prismagram-backendd.herokuapp.com/"
 }
 
 export default options
